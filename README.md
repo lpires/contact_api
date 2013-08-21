13cc156be23d1bd213b274b5c47a61c3ffccb4ac
Leila Pires <Leila.Pires@gmail.com>
2013-08-21 08:07:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+contact_api_app
+===============
+
+3nd Party API connector
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = 'ecfdb8317205edfb3ee64e5eeb27c4206ea05b8804a683c5092399fd55ad91c6f47d430ad9b02549d567c756c885a98b7417f5191829bfa7904334f0a6b2adac'
